f18f8443d00a2e562b91ec956acf8db3e148c400
LAHardman <122503446+LAHardman@users.noreply.github.com>
2024-03-20 15:24:10 -0400

    updated class selectors

--- classPage.js
+++ classPage.js
@@ -1,14 +1,14 @@
 function classSearch() {
     let input = document.getElementById('classSearch').value
     input = input.toLowerCase();
-    let x = document.getElementsByClassName('classSelection');
+    let x = document.getElementsByClassName('classLabel');
     let l = x.length;
     for (i = 0; i < l; i++) {
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].classList.add("hidden");
+        x[i].parentElement.classList.add("hidden");
         }
         else {
-        x[i].classList.remove("hidden");
+        x[i].parentElement.classList.remove("hidden");
         }
     }
 }
