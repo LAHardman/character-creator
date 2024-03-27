b98b4819cf75938f1e3bf1297ca74338c69ea273
LAHardman <122503446+LAHardman@users.noreply.github.com>
2024-03-27 09:57:12 -0400

    Started impelmenting popups for selections

--- javascript/elements/selection.js
+++ javascript/elements/selection.js
@@ -6,14 +6,18 @@ for (let i = 0; i < selections.length; i++) {
 }
 
 function selectOption(selectedOption) {
-    let previousSelection = document.getElementById('selected')
+    let popout = selectedOption.getElementsByClassName('popout')[0];
+    let previousSelection = document.getElementById('selected');
     if (previousSelection) {
+        let previousPopout = previousSelection.getElementsByClassName('popout')[0];
         previousSelection.removeAttribute('id');
+        previousPopout.classList.remove('active');
         if(selectedOption!=previousSelection) {
             selectedOption.id = 'selected';
+            popout.classList.add('active');
         }
     } else {
         selectedOption.id = 'selected';
-
+        popout.classList.add('active');
     }
 }
